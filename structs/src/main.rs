350a0b8c7476881561601c91cfef1ee8ce6f31e9
Nikola Totev <nikola.r.totev@gmail.com>
2025-09-22 18:14:57 +0300

    feat: add structs

---
+++ structs/src/main.rs
@@ -0,0 +1,34 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hole(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("Area of rect is {0}", rect1.area());
+    println!("Rect info: {rect1:?}");
+    println!("Debug rect info:");
+    dbg!(&rect1);
+}
