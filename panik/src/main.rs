6aa431359ca2ea08816548394bf248d45ef4257d
Nikola Totev <nikola.r.totev@gmail.com>
2025-10-03 21:41:03 +0300

    feat: working with files

--- panik/src/main.rs
+++ panik/src/main.rs
@@ -1,5 +1,7 @@
+use std::{fs::File, io::ErrorKind};
+
 fn main() {
-    let panik_type = String::from("manual_panic");
+    let panik_type = String::from("file_ops");
 
     match &panik_type[..] {
         "out_of_bounds" => {
@@ -10,8 +12,24 @@ fn main() {
         "manual_panic" => {
             panic!("I saw a spider!")
         }
+        "file_ops" => {
+            let greeting_file_result = File::open("hello.txt");
+
+            let greeting_file = match greeting_file_result {
+                Ok(file) => file,
+                Err(error) => match error.kind() {
+                    ErrorKind::NotFound => match File::create("hello.txt") {
+                        Ok(fc) => fc,
+                        Err(e) => panic!("Problem creating the file: {e:?}"),
+                    },
+                    _ => {
+                        panic!("Problem opening the file: {error:?}");
+                    }
+                },
+            };
+        }
         _ => {
             println!("Ops")
         }
-    }    
+    }
 }
